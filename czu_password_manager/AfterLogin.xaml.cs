c8d08fca7185e7cfd59030019571e86a09406e39
ar3s-xss <aresgodxss@gmail.com>
2024-12-03 23:33:46 +0100

    functional adding of credentials

--- czu_password_manager/AfterLogin.xaml.cs
+++ czu_password_manager/AfterLogin.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace czu_password_manager
 {
@@ -19,14 +23,81 @@ namespace czu_password_manager
     /// </summary>
     public partial class AfterLogin : Window
     {
+        public ObservableCollection<Credential> Credentials { get; set; }
         public AfterLogin()
         {
             InitializeComponent();
+            
+            Credentials = new ObservableCollection<Credential>();
+            LoadCredentials();
+            DataContext = this;
+            
         }
 
         public void ChangeLabel(string message)
         {
-            label2.Content = message;
+            //label2.Content = message;
+        }
+
+        private void AddNew(object sender, RoutedEventArgs e)
+        {
+            addNew.Visibility = Visibility.Visible;
+        }
+
+        private void LoadCredentials()
+        {
+            string vaultPath = "vault.xml";
+            Credentials = new ObservableCollection<Credential>();
+            if (File.Exists(vaultPath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Credential>));
+                using (StreamReader reader = new StreamReader(vaultPath)) 
+                {
+                    var loadedCredentials = (ObservableCollection<Credential>)serializer.Deserialize(reader);
+                    Credentials.Clear();
+                    foreach (var credential in loadedCredentials)
+                    {
+                        Credentials.Add(credential);
+                    }
+                }
+
+            }
+
+        }
+        private void SaveCredentials(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                string vaultName = "vault.xml";
+
+                Credentials.Add(new Credential
+                {
+                    name = nameInput.Text,
+                    username = usernameInput.Text,
+                    password = passwordInput.Password
+                });
+                
+                XmlSerializer serializer2 = new XmlSerializer(typeof(ObservableCollection<Credential>));
+                using(StreamWriter writer = new StreamWriter(vaultName, false))
+                {
+                    serializer2.Serialize(writer, Credentials);
+                }
+
+                nameInput.Clear();
+                usernameInput.Clear();
+                passwordInput.Clear();
+                addNew.Visibility = Visibility.Collapsed;
+                credentialsListBox.ItemsSource = null;
+                credentialsListBox.ItemsSource = Credentials;
+
+
+            } catch(Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+            
+
         }
     }
 }
---
+++ czu_password_manager/Vault.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace czu_password_manager
+{
+    internal class Vault
+    {
+        
+        
+    }
+    public class Credential
+    {
+        public string name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+    
+    
+
+
+}
